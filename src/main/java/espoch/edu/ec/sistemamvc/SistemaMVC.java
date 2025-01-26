bf8e8a086f3d585ba753628e8771fb0d53582f5e
andrea yamileth <andreachaquinga2005@gmail.com>
2025-01-26 18:51:31 -0500

    h

---
+++ src/main/java/espoch/edu/ec/sistemamvc/SistemaMVC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.sistemamvc;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class SistemaMVC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
